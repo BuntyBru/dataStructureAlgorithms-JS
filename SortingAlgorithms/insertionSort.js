83ec3118aac4d8c3bac4c0cbf956db33db79e8b4
Buntybru <buntybru@gmail.com>
2019-06-19 00:13:03 +0530

    insertionSort

--- SortingAlgorithms/insertionSort.js
+++ SortingAlgorithms/insertionSort.js
@@ -4,12 +4,9 @@ function insertionSort(arr)
 	for (var i=1;i<arr.length;i++)
 	{
 		let currentValue = arr[i];
-		for (var j=i-1;j>=0;j--)
+		for (var j=i-1;j>=0 && arr[j] > currentValue;j--)
 		{
-			if ( arr[j] > currentValue)
-			{
-				arr[j+1]=arr[j]
-			}
+			arr[j+1]=arr[j];	
 		}
 		arr[j+1] = currentValue;
 	}
