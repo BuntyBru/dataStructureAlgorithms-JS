89089ea0949ed9cb80d5692d5bfc51864a72d633
Buntybru <buntybru@gmail.com>
2019-06-19 14:56:17 +0530

    mergeSort

--- SortingAlgorithms/mergeSort.js
+++ SortingAlgorithms/mergeSort.js
@@ -1,14 +1,8 @@
-var arr1 = [1,22,33,44];
-var arr2 = [-1,2];
-
-
-
 function merge(arr1,arr2)
 {
 	var i=0;
 	var j=0;
 	var arr3=[];
-
 	while (i < arr1.length && j <arr2.length)
 	{
 		if(arr1[i] < arr2[j])
@@ -22,8 +16,6 @@ function merge(arr1,arr2)
 			j = j+1;
 		}
 	}
-	console.log(i,j,arr3);
-
 	if(i < arr1.length)
 	{
 		for(var p=i ;p<arr1.length;p++)
@@ -38,12 +30,26 @@ function merge(arr1,arr2)
 			arr3.push(arr2[q]);
 		}
 	}
-
-
-
-
 	return arr3;
-
 }
 
-console.log(merge(arr1,arr2));
+//console.log(merge(arr1,arr2));
+
+
+function mergeSort(arr)
+{
+	if (arr.length <=1)
+	{
+		return arr;
+	}
+	else
+	{
+	let mid = Math.floor(arr.length/2);
+	let lefttSide = mergeSort(arr.slice(0,mid));
+	let rightSide = mergeSort(arr.slice(mid));
+	return merge(lefttSide,rightSide);
+	}
+}
+
+let arr1 = [5,3,4,2,11,1,9,7,4,3,2,111];
+console.log(mergeSort(arr1));
