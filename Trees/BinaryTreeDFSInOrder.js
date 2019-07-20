61cf74296056b9fa8327c3d89bb9c02651df0d3f
Buntybru <buntybru@gmail.com>
2019-07-21 03:05:15 +0530

    Some changes

---
+++ Trees/BinaryTreeDFSInOrder.js
@@ -0,0 +1,94 @@
+class Node
+{
+	constructor(value)
+	{
+		this.val = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree
+{
+	constructor()
+	{
+		this.root = null;
+	}
+
+	insert(value)
+	{
+		let newNode = new Node(value);
+
+		if(!this.root)
+		{
+			this.root = newNode;
+			return this;
+		}
+
+		else
+		{
+			let currentNode = this.root;
+			while(true)
+			{
+				if(value < currentNode.val )
+				{
+					//go left
+					if(currentNode.left === null)
+					{
+						currentNode.left = newNode;
+						return this;
+					}
+					else
+					{
+						currentNode = currentNode.left;
+					}
+				}
+
+				else if(value > currentNode.val)
+				{
+					//go right
+					if(currentNode.right === null)
+					{
+						currentNode.right = newNode;
+						return this;
+					}
+					else
+					{
+						currentNode = currentNode.right;
+					}
+				}
+
+				else
+				{
+					return undefined;
+				}
+			}
+		}
+	}
+
+	inOrder()
+	{
+		var data =[];
+		function traverse(node)
+		{
+			if(node.left) traverse(node.left);
+			data.push(node.val);
+			if(node.right) traverse(node.right);
+			
+		}
+
+		traverse(this.root);
+		return data;
+		
+	}
+}
+
+let t = new BinarySearchTree();
+t.insert(10);
+t.insert(6);
+t.insert(15);
+t.insert(3);
+t.insert(8);
+t.insert(20);
+// 3,6,8,10,15,20
+console.log(t.inOrder());
